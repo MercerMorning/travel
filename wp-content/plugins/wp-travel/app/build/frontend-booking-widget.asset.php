b9e4fa7e09111fb757d52c00c1492ae0f6706368
MercerMorning <hugopochta@gmail.com>
2020-08-29 11:54:27 +0300

    'commit'

---
+++ wp-content/plugins/wp-travel/app/build/frontend-booking-widget.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('moment', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '45b618dc69911cabae7d6447e319112e');
---
+++ wp-content/uploads/mc4wp-debug-log.php
@@ -0,0 +1,2 @@
+<?php exit; ?>
+[2020-08-20 00:08:27] WARNING: Custom > No Mailchimp lists were selected
